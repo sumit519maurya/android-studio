641261e3aff43e50c9e5207eb440596d76bd664e
sumit519maurya <sumit51990maurya@gmail.com>
2024-07-24 02:53:04 +0530

    first update navigation ki maaki chuut commit

--- app/src/main/java/com/example/washmart/homepage.kt
+++ app/src/main/java/com/example/washmart/homepage.kt
@@ -1,16 +1,24 @@
 package com.example.washmart
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_homepage)
+        val main:DrawerLayout=findViewById(R.id.main)
+        val nav:NavigationView=findViewById(R.id.nav)
+        nav.setOnClickListener{
+            main.open()
+        }
 
     }
 }
--- app/src/main/java/fragadapter.kt
+++ app/src/main/java/fragadapter.kt
@@ -2,19 +2,17 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.washmart.login
-import com.example.washmart.pickup2
 import com.example.washmart.timingfragment
 
 class fragadapter (fa:FragmentActivity): FragmentStateAdapter(fa){
     override fun getItemCount(): Int {
-      return 3
+      return 2
     }
 
     override fun createFragment(position: Int): Fragment {
        return when(position){
            0->timingfragment()
-           1->pickup2()
-           2->login()
+           1->login()
            else->timingfragment()
        }
     }
