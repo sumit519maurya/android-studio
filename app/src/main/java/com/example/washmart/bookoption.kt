2af3161fef486818068f5489c7d2c70925f84c07
sumit519maurya <sumit51990maurya@gmail.com>
2024-08-02 02:17:12 +0530

    total

--- app/src/main/java/com/example/washmart/bookoption.kt
+++ app/src/main/java/com/example/washmart/bookoption.kt
@@ -2,9 +2,12 @@ package com.example.washmart
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -17,42 +20,32 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class bookoption : AppCompatActivity() {
-    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var shirtqt: TextView
+    private lateinit var tshirtqt: TextView
+    private lateinit var pantqt: TextView
+    private lateinit var shortqt: TextView
+    private var count=0
+    private var count2=0
+    private var count3=0
+    private var count4=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_bookoption)
         val back:Button=findViewById(R.id.backbtn)
         val next:Button=findViewById(R.id.nextbtn)
-        val drawerLayout: DrawerLayout = findViewById(R.id.main)
-        val navigation: NavigationView = findViewById(R.id.navigation)
-        val tolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(tolbar)
-        toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            tolbar,
-            R.string.opendrawer,
-            R.string.closedrawer
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-         navigation.setNavigationItemSelectedListener { item ->
-             val id = item.itemId
-             if (id == R.id.home) {
-                 Toast.makeText(this, "sdsfsf", Toast.LENGTH_LONG).show()
+        val shirt:CheckBox=findViewById(R.id.shirt)
+        val tshirt:CheckBox=findViewById(R.id.tshirt)
+        val pants:CheckBox=findViewById(R.id.pants)
+        val shorts:CheckBox=findViewById(R.id.shorts)
+        val total:Button=findViewById(R.id.total)
+        val totaltext:TextView=findViewById(R.id.totaltext)
+         shirtqt=findViewById(R.id.shirtqt)
+         tshirtqt=findViewById(R.id.tshirtqt)
+         pantqt=findViewById(R.id.paintqt)
+         shortqt=findViewById(R.id.shortqt)
 
-             } else if (id == R.id.profile) {
-             }
-             else if (id == R.id.order) {
-             }
-             else if (id == R.id.help) {
-             }
-             else if (id == R.id.logout) {
-             }
-             drawerLayout.closeDrawer(GravityCompat.START)
-             true
-         }
+
        back.setOnClickListener {
            val i= Intent(this,homepage::class.java)
            startActivity(i)
@@ -61,5 +54,79 @@ class bookoption : AppCompatActivity() {
             val i= Intent(this,orderpage2::class.java)
             startActivity(i)
         }
+        total.setOnClickListener {
+            var total1 = 0
+            var isValid = true
+
+            if (shirt.isChecked) {
+                val quantity = shirtqt.text.toString().toIntOrNull()
+                if (quantity != null && quantity > 0){
+                total1 += 10 * quantity
+            }else{
+                isValid=false
+                }
+            }
+            if (tshirt.isChecked) {
+                val quantity = tshirtqt.text.toString().toIntOrNull()
+                if (quantity != null && quantity > 0){
+                    total1 += 20 * quantity
+                }else{
+                    isValid=false
+                }
+            }
+            if (pants.isChecked) {
+                val quantity = pantqt.text.toString().toIntOrNull()
+                if (quantity != null && quantity > 0){
+                    total1 += 25 * quantity
+                }else{
+                    isValid=false
+                }
+            }
+            if (shorts.isChecked) {
+                val quantity = shortqt.text.toString().toIntOrNull()
+                if (quantity != null && quantity > 0){
+                    total1 += 15 * quantity
+                }else{
+                    isValid=false
+                }
+            }
+            if (isValid){
+                totaltext.text="Total:-$total1"
+            }else{
+                Toast.makeText(this,"Please Enter The Quantity",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    fun add(v: View) {
+        count++
+        shirtqt.text=count.toString()
+    }
+    fun minus(v: View) {
+        if (count <= 0) count = 0 else count--
+        shirtqt.text = count.toString()
+    }
+    fun add2(v: View) {
+        count2++
+        tshirtqt.text=count2.toString()
+    }
+    fun minus2(v: View) {
+        if (count2 <= 0) count2= 0 else count2--
+        tshirtqt.text = count2.toString()
+    }
+    fun add3(v: View) {
+        count3++
+        pantqt.text=count3.toString()
+    }
+    fun minus3(v: View) {
+        if (count3 <= 0) count3 = 0 else count3--
+        pantqt.text = count3.toString()
+    }
+    fun add4(v: View) {
+        count4++
+        shortqt.text=count4.toString()
+    }
+    fun minus4(v: View) {
+        if (count4 <= 0) count4 = 0 else count4--
+        shortqt.text = count4.toString()
     }
 }
